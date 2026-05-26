4d10c09c484072528ff3f0f4357953e9e320e5de
Mattia Domizio <mat.dom1609job@gmail.com>
2026-05-26 13:25:07 +0200

    POST: trigger

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -3,7 +3,7 @@ export const about = {
     'I\'m Mattia, a passionate Computer Engineer. I enjoy building things, learning new technologies and sharing knowledge. I have some experience in Frontend and Backend development, DevOps, Distributed Systems, Big Data, Low-level Programming and Game Development. I\'m always looking for new challenges and opportunities to grow as a developer and as a person. Look at my projects and notes to see what I\'ve worked on, and at my CV to see more details about my experience!',
 };
 
-
+// ciao
 export const terminalContacts = {
   github: 'https://github.com/Koisuji02',
   linkedin: 'https://linkedin.com/in/mattia-domizio-7a07b628b',
